0e28b4de60266ab0193dba3e9c41c9497c591eff
Sergey Stenyukov <stenyukovs@gmail.com>
2021-08-12 20:35:12 +0300

    Main method cleared

--- src/main/java/com/opendev/odtask/OdTask.java
+++ src/main/java/com/opendev/odtask/OdTask.java
@@ -2,10 +2,6 @@ package com.opendev.odtask;
 
 import com.opendev.odtask.connection.ConnectionProvider;
 import com.opendev.odtask.connection.ScriptReader;
-import com.opendev.odtask.core.dao.EquipmentDao;
-import com.opendev.odtask.core.dao.WellDao;
-import com.opendev.odtask.core.dao.impl.EquipmentDaoImpl;
-import com.opendev.odtask.core.dao.impl.WellDaoImpl;
 import com.opendev.odtask.ui.UserInterface;
 
 public class OdTask {
@@ -23,14 +19,6 @@ public class OdTask {
         ScriptReader scriptReader = new ScriptReader();
         scriptReader.readSqlScript(schema, DB_URL);
         scriptReader.readSqlScript(data, DB_URL);
-
-        EquipmentDao equipmentDao = new EquipmentDaoImpl(connectionProvider);
-        WellDao wellDao = new WellDaoImpl(connectionProvider, equipmentDao);
-
-        equipmentDao.findAll().forEach(System.out::println);
-
-        wellDao.findAll().forEach(System.out::println);
-        System.out.println(wellDao.findWellIdByName("North"));
 
         UserInterface ui = new UserInterface(connectionProvider);
         ui.run();
